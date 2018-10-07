b402c4cb1f4c5717e4dce45976ba62386cf373d2
Nyalls Hemingway <nyallshemingway@c192008.laplan.net.ed.ac.uk>
2018-10-07 10:39:59 +0100

    added some erb files, inc. layout and index for each model

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,9 @@
-require( 'sinatra' )
-require( 'sinatra/contrib/all' )
-require_relative('controllers/merchants_controller.rb')
-require_relative('controllers/tags_controller.rb')
-require_relative('controllers/transactions_controller.rb')
+require( "sinatra" )
+require( "sinatra/contrib/all")
+require_relative("controllers/merchants_controller.rb")
+require_relative("controllers/tags_controller.rb")
+require_relative("controllers/transactions_controller.rb")
 
 get '/' do
-  erb( :index )
+  erb(:index)
 end
---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,7 @@
+require( "sinatra" )
+require( "sinatra/contrib/all" )
+require( "pry-byebug" )
+require_relative( "../models/merchant.rb" )
+require_relative( "../models/tag.rb")
+require_relative("../models/transaction.rb")
+also_reload( "../models/*" )
