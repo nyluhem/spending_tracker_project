33d12880c9f27a3ebf8aafc3cb6aabfc1b169748
Nyalls Hemingway <nyallshemingway@Nyallss-MacBook-Pro.local>
2018-10-08 14:17:54 +0100

    completed edit/update restful routes for merchants and tags

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -6,21 +6,38 @@ require_relative( "../models/tag.rb")
 require_relative("../models/transaction.rb")
 also_reload( "../models/*" )
 
+#INDEX
+
 get("/merchants") do
   @merchants = Merchant.all
   erb(:"merchants/index")
 end
 
+#NEW
+
 get("/merchants/new") do
   erb(:"merchants/new")
 end
 
-get("/merchants/edit") do
+#EDIT
+
+get("/merchants/:id/edit") do
+  @merchant = Merchant.find(params[:id])
   erb(:"merchants/edit")
 end
+
+#CREATE
 
 post("/merchants") do
   @merchant = Merchant.new(params)
   @merchant.save
   redirect to ("/merchants")
+end
+
+#UPDATE
+
+post("/merchants/:id") do
+  merchant = Merchant.new(params)
+  merchant.update
+  redirect to "/merchants"
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,21 +6,38 @@ require_relative( "../models/tag.rb")
 require_relative("../models/transaction.rb")
 also_reload( "../models/*" )
 
+#INDEX
+
 get("/tags") do
   @tags = Tag.all
   erb(:"tags/index")
 end
 
+#NEW
+
 get("/tags/new") do
   erb(:"tags/new")
 end
 
-get("tags/edit") do
+#EDIT
+
+get("/tags/:id/edit") do
+  @tag = Tag.find(params[:id])
   erb(:"tags/edit")
 end
+
+#CREATE
 
 post("/tags") do
   @tag = Tag.new(params)
   @tag.save
+  redirect to "/tags"
+end
+
+#UPDATE
+
+post("/tags/:id") do
+  tag = Tag.new(params)
+  tag.update
   redirect to "/tags"
 end
